cef1f5b6d428bba68c19671fda85d1fe620e860f
Orit Kozolin <orit@nachum.co.il>
2019-11-26 15:07:39 +0200

    use *ngIf to check if there are any posts records to display different output

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -6,9 +6,10 @@ import { Component } from "@angular/core";
   styleUrls: ["./post-list.component.css"]
 })
 export class PostListComponent {
-  myposts = [
-    { title: "First Post", content: "This is the first post's content" },
-    { title: "Second Post", content: "This is the second post's content" },
-    { title: "Third Post", content: "This is the third post's content" }
-  ];
+  // myposts = [
+  //   { title: "First Post", content: "This is the first post's content" },
+  //   { title: "Second Post", content: "This is the second post's content" },
+  //   { title: "Third Post", content: "This is the third post's content" }
+  // ];
+  myposts = [];
 }
