5848f9525a5c3266b9e66eae8508f36cc5355621
Orit Kozolin <orit@nachum.co.il>
2019-11-26 15:35:11 +0200

    create 'emit' event of adding new post

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,14 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter } from "@angular/core";
 @Component({
   selector: "app-post-create",
   templateUrl: "./post-create.component.html",
   styleUrls: ["./post-create.component.css"]
 })
 export class PostCreateComponent {
-  enteredValue = "";
-  newPost = "NO CONTENT";
+  enteredTitle = "";
+  enteredContent = "";
+  postCreated = new EventEmitter();
   onAddPost(postInput: HTMLAreaElement) {
-    console.dir(postInput);
-    this.newPost = this.enteredValue;
+    const post = {
+      title: this.enteredTitle,
+      content: this.enteredContent
+    };
+    this.postCreated.emit(post);
   }
 }
