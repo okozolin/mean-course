c0664b6382bf90f04006af10c989a8cd54253512
Orit Kozolin <orit@nachum.co.il>
2019-12-10 15:35:08 +0200

    option 2 - using @Injectable ,allow now Angular to find the service. This way only one instance of the service is created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,6 @@ import { AppComponent } from "./app.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { PostListComponent } from "./posts/post-list/post-list.component";
 import { HeaderComponent } from "./header/header.component";
-import { PostsService } from "./posts/posts.service";
 
 @NgModule({
   declarations: [
@@ -35,7 +34,7 @@ import { PostsService } from "./posts/posts.service";
     MatToolbarModule,
     MatExpansionModule
   ],
-  providers: [PostsService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,14 +1,16 @@
-import { Post } from './post.model';
+import { Injectable } from "@angular/core";
+import { Post } from "./post.model";
 
+@Injectable
 export class PostsService {
   private posts: Post[] = [];
 
   getPosts() {
-    return [...this.posts];  // this would make the original array immutable
+    return [...this.posts]; // this would make the original array immutable
   }
 
   addPost(title: string, content: string) {
-    const post: Post =  {title, content}; // es6 shorthand for {title: title, content: content}
+    const post: Post = { title, content }; // es6 shorthand for {title: title, content: content}
     this.posts.push(post);
   }
 }
