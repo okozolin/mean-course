0fce35c725d6e12e15e4d7b8484a14aa45f02559
Orit Kozolin <orit@nachum.co.il>
2019-12-24 17:38:20 +0200

    use rxjs to update the array of posts with new added post

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+
 import { Post } from "./post.model";
 
 @Injectable({ providedIn: "root" })
 export class PostsService {
   private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
 
   getPosts() {
-    return this.posts; // not the cleanest solution but works
+    return [...this.posts]; // this would make the original array immutable
   }
 
   addPost(title: string, content: string) {
     const post: Post = { title, content }; // es6 shorthand for {title: title, content: content}
     this.posts.push(post);
+    this.postsUpdated.next([...this.posts]);
   }
 }
