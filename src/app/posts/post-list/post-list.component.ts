142b12e22f73849acc064352bf6b95a3b3f06e0b
Orit Kozolin <orit@nachum.co.il>
2019-12-24 17:58:26 +0200

    clean code: to prevent memory leackage when the component page is not in the DOM, use unsubscribe the Observables to destroy it

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+
 import { Post } from "../post.model";
 import { PostsService } from "../posts.service";
 
@@ -7,13 +9,14 @@ import { PostsService } from "../posts.service";
   templateUrl: "./post-list.component.html",
   styleUrls: ["./post-list.component.css"]
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
   // myposts = [
   //   { title: "First Post", content: "This is the first post's content" },
   //   { title: "Second Post", content: "This is the second post's content" },
   //   { title: "Third Post", content: "This is the third post's content" }
   // ];
   myposts: Post[] = [];
+  private postsSub: Subscription;
 
   // dependency injection - injecting the service -
   // adding 'public' will automaticly create the variable 'postService' on the class
@@ -21,8 +24,14 @@ export class PostListComponent implements OnInit {
   constructor(public postsService: PostsService) {}
   ngOnInit() {
     this.myposts = this.postsService.getPosts();
-    this.postsService.getPostUpdatedListener().subscribe((posts: Post[]) => {
-      this.myposts = posts;
-    });
+    this.postsSub = this.postsService
+      .getPostUpdatedListener()
+      .subscribe((posts: Post[]) => {
+        this.myposts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe(); // remove the subscription to prevent memory leacks
   }
 }
